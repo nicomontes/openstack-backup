da8efa26f9c0b1db85d22d2f86db47ecaa932f23
root <root@ubuntu-Master.(none)>
2013-11-29 07:43:09 +0100

    Change new Backup and last Backup

--- OpenStack-Backup.sh
+++ OpenStack-Backup.sh
@@ -3,14 +3,13 @@
 # Configure Config File
 glance_config="/home/satin/openstack-backup/glanceBackup.config"
 
-# Display Date with syslog format
-date +"%h %d %H:%M:%S"
+# Get Log
+log=$(cat $glance_config | grep "^log" | grep -E -o "[^=]*$" | head -1)
 
 # Get Latest Version
-script_dir=$(cat $glance_config | grep "^script_dir" | grep -E -o "[^=]*$" | head -1)
-cd $script_dir
+cd /home/satin/openstack-backup/
 git_pull=$(git pull)
-echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: $git_pull" >> $HOME/VMBackup/log
+echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: Start with Git Pull : $git_pull" >> $log
 
 # Variables
 admin_user=$(cat $glance_config | grep "^admin_username" | grep -E -o "[^=]*$" | head -1)
@@ -18,17 +17,39 @@ admin_pass=$(cat $glance_config | grep "^admin_password" | grep -E -o "[^=]*$" |
 admin_tenant_ID=$(cat $glance_config | grep "^admin_tenantId" | grep -E -o "[^=]*$" | head -1)
 API_endpoint_keystone=$(cat $glance_config | grep "^api_endpoint_identity_service" | grep -E -o "[^=]*$" | head -1)
 API_endpoint_compute=$(cat $glance_config | grep "^api_endpoint_compute_service" | grep -E -o "[^=]*$" | head -1)
-backup_list=$(cat $glance_config | grep -E "[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}$")
+backup_list=$(cat $glance_config | grep -E "^[^\ #].*[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}$")
 glance_images_folder=$(cat $glance_config | grep "^glance_images_folder" | grep -E -o "[^=]*$" | head -1)
 sources_file=$(cat $glance_config | grep "^sources_file" | grep -E -o "[^=]*$" | head -1)
+
+# Sources for Glance commands
+echo "source file : $sources_file" >> $log
+source $sources_file
 
 # Get token (Admin account for all access)
 token=$(curl -s -k -X 'POST' $API_endpoint_keystone/tokens \
 -d '{"auth":{"passwordCredentials":{"username": "'$admin_user'", "password": "'$admin_pass'"}, "tenantId": "'$admin_tenant_ID'"}}' \
 -H 'Content-type: application/json'|grep -E -o '[A-Za-z0-9\+\-]{100,}')
+echo "token GET" >> $log
+
+# Last Backup ID
+for line in $backup_list
+do
+  backup_name=$(echo $line | cut -f1 -d ";" | grep -E -o "[A-Za-z0-9]*")
+  # create regex for match all Backup ID
+  regex_list="$regex_list $backup_name |"
+done
+regex_list=$(echo $regex_list | sed "s/.$//")
+# Exec regex for match all Backup IP
+last_backup_list=$(nova image-list | grep -E "$regex_list" | cut -f2 -d "|" | grep -o "[^\ ]*")
+# Push in array all backup ID
+for line in $last_backup_list
+do
+  last_backup[${#last_backup[*]}]=$line
+done
 
 # Launch Backup for each VM
-for line in $(echo $backup_list)
+echo "backup list : $backup_list" >> $log
+for line in $backup_list
 do
   backup_name=$(echo $line | cut -f1 -d ";" | grep -E -o "[A-Za-z0-9]*")
   backup_type=$(echo $line | cut -f2 -d ";" | grep -E -o "[A-Za-z0-9]*")
@@ -37,21 +58,17 @@ do
   curl -s -d '{"createBackup": {"name": "'$backup_name'","backup_type": "'$backup_type'","rotation": '$backup_rotation'}}' \
   -H "X-Auth-Token: $token " \
   -H "Content-type: application/json" $API_endpoint_compute/servers/$backup_id/action
+  echo "VM : $backup_name : $backup_id : sendbackup on API" >> $log
   sleep 5
 done
-
-# Sources for Glance commands
-source $sources_file 
-
-# New Backup ID
-new_backup_ID=$(glance image-list|grep -E -o ".*BackupAutoScript.*queued"|grep -E -o "[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}")
-echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: New Backup : $new_backup_ID">> $HOME/VMBackup/log
+sleep 5
 
 # Sleep when Create Backup Process
 VM_queued=$(glance image-list|grep -E -o ".*BackupAutoScript.*queued" | wc -c)
 while [ $VM_queued -gt 0 ]
 do
   sleep 5
+  echo "VM Queued : $VM_queued" >> $log
   VM_queued=$(glance image-list|grep -E -o ".*BackupAutoScript.*queued" | wc -c)
 done
 
@@ -60,21 +77,50 @@ VM_saving=$(glance image-list|grep -E -o ".*BackupAutoScript.*saving" | wc -c)
 while [ $VM_saving -gt 0 ]
 do
   sleep 5
+  echo "VM Saving : $VM_saving" >> $log
   VM_saving=$(glance image-list|grep -E -o ".*BackupAutoScript.*saving" | wc -c)
 done
-echo "Backup Saved"
+echo "Backup Saved" >> $log
+
+sleep 10
+
+# New Backup ID
+# Exec regex for match all NEW backup ID
+new_backup_list=$(nova image-list | grep -E "$regex_list" | cut -f2 -d "|" | grep -o "[^\ ]*")
+echo "New Backup :" $new_backup_list >> $log
+# Read Arry
+i_max=$(echo $new_backup_list | wc -w)
+for ((i = 0; i < $i_max; i += 1))
+do
+  for line in $new_backup_list
+  do
+    # IF new Backup is in last backup list THEN add in regex
+    if [ "$line" = "${last_backup[$i]}" ]
+    then
+      regex_match_new_backup=$(echo "$regex_match_new_backup $line |")
+    fi
+  done
+done
+regex_match_new_backup=$(echo $regex_match_new_backup | sed "s/.$//")
+echo $new_backup_list
+echo "---"
+echo $regex_match_new_backup
+# Match Regex (Same Backup ID) and we invert with -v
+echo "+++"
+echo $new_backup_list | grep -E -v $regex_match_new_backup
 
 # Copy VM
-for VM_ID in $new_backup_ID
+for VM_ID in $new_backup_list
 do
-  cp $glance_images_folder$VM_ID $HOME/VMBackup/
+  echo "$glance_images_folder$VM_ID"
+  cp $glance_images_folder$VM_ID
   if [[ $(echo $?) -gt 0 ]]
   then
-    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: VM : $VM_ID check in /glance/images/ ERROR">> $HOME/VMBackup/log
+    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: VM : $VM_ID check in /glance/images/ ERROR">> $log
 
   else
-    rm -f $HOME/VMBackup/$VM_ID
-    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: VM : $VM_ID Copied">> $HOME/VMBackup/log 
+    rm -f $$VM_ID
+    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: VM : $VM_ID Copied">> $log 
   fi
 done
 
@@ -84,11 +130,11 @@ do
   ls $glance_images_folder$VM_ID $1>/dev/null
   if [[ $(echo $?) -gt 0 ]]
   then
-    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: VM : $VM_ID aren't in Glance images folder">> $HOME/VMBackup/log
-    rm -f $HOME/VMBackup/$VM_ID
-    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: $VM_ID Removed" >> $HOME/VMBackup/log
+    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: VM : $VM_ID aren't in Glance images folder">> $log
+    rm -f $$VM_ID
+    echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: $VM_ID Removed" >> $log
   fi
 done
 
 # Log in Syslog File
-echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: Backup Finished" >> $HOME/VMBackup/log
+echo "$(date +"%h %d %H:%M:%S") $HOSTNAME Openstack_Backup: Backup Finished" >> $log
